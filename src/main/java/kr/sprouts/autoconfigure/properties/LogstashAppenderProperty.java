cfab3639ed51de0d7532eb1b38814e21a46ebc37
Jinwon Seo <jwseo@live.co.kr>
2023-06-23 15:35:00 +0900

    Add logstash appender property.

--- src/main/java/kr/sprouts/autoconfigure/properties/LogstashAppenderProperty.java
+++ src/main/java/kr/sprouts/autoconfigure/properties/LogstashAppenderProperty.java
@@ -1,16 +1,24 @@
 package kr.sprouts.autoconfigure.properties;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
 
 @ConfigurationProperties(prefix = "sprouts.logstash-appender")
 public class LogstashAppenderProperty {
-    private String id;
+    @Getter @Setter
+    private String name;
+    @Getter @Setter
+    private String identifier;
+    @Getter @Setter
+    private List<Destination> destinations;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
+    public static class Destination {
+        @Getter @Setter
+        private String host;
+        @Getter @Setter
+        private Integer port;
     }
 }
